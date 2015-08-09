1f8f7bb7855fa20e3a1d2b972cf6904edabb6d83
Remnaut <Remnaut@users.noreply.github.com>
2015-08-09 12:37:52 -0500

    Initial Commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public float ballInitialVelocity = 600f;
+
+	private bool ballInPlay = false;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetButtonDown("Jump") && ballInPlay == false)
+		{
+			ballInPlay = true;
+			rb.isKinematic = false;
+			rb.AddForce(new Vector3(ballInitialVelocity*Mathf.Sign(Random.Range(-1f,1f)), ballInitialVelocity*Random.Range(-1f,1f), 0));
+
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerPaddleController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPaddleController : MonoBehaviour {
+
+	public float paddleSpeed = 1f;
+	public float clamp = 8f;
+
+
+	private Vector3 playerPos = new Vector3 (-13f, 0, 0);
+
+	// Update is called once per frame
+	void Update () 
+	{
+		float yPos = transform.position.y + Input.GetAxis ("Vertical") * paddleSpeed;
+		playerPos = new Vector3 (-13f, Mathf.Clamp (yPos, -clamp, clamp), 0);
+		transform.position = playerPos;
+	}
+}
